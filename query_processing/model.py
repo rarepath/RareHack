a86fb228ddbc790297404abfa20e0d971ceedc52
Sundareswar Pullela <pullelas@oregonstate.edu>
2024-06-19 14:52:25 -0700

    fix model loading issue

--- query_processing/model.py
+++ query_processing/model.py
@@ -8,7 +8,7 @@ from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
     
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map = "auto", trust_remote_code=True)
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens = 1000)
 llama_hf = HuggingFacePipeline(pipeline=pipe)
 
