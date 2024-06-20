61a1e53b9789369fb9362498e6077da4a25537ae
Sundareswar Pullela <pullelas@oregonstate.edu>
2024-06-19 18:59:13 -0700

    temperature

--- query_processing/model.py
+++ query_processing/model.py
@@ -9,7 +9,7 @@ quantization_config = QuantoConfig(weights="float8")
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map = "auto", trust_remote_code=True, quantization_config=quantization_config)
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens = 1000)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens = 1000, temperature=0.2)
 llama_hf = HuggingFacePipeline(pipeline=pipe)
 
 
