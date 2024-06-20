ef3b73a83474cfe4a243f9891bbcd24ff6c9f2ea
Sundareswar Pullela <pullelas@oregonstate.edu>
2024-06-19 17:57:22 -0700

    Added quantization

--- query_processing/model.py
+++ query_processing/model.py
@@ -1,14 +1,14 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, QuantoConfig
 import torch
 from model_config import model_name
 from langchain_huggingface import HuggingFacePipeline
 
 
 
-    
+quantization_config = QuantoConfig(weights="float8")
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map = "auto", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map = "auto", trust_remote_code=True, quantization_config=quantization_config)
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens = 1000)
 llama_hf = HuggingFacePipeline(pipeline=pipe)
 
