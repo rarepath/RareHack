c4de00f9c0ef287d08b19c7f3a0e564169f1c670
derrickjhiggins <derrick.higgins@outlook.com>
2024-06-21 08:35:18 -0500

    add text bubbles. Update error message.

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -60,7 +60,7 @@ export class ChatComponent {
             error: error => {
                 console.error('Error fetching chatbot response:', error);
                 let errorString: string = "I'm sorry, I'm experiencing technical difficulties right now. Please try again."
-                this.appendMessage("error", [errorString])
+                this.appendMessage("chat", [errorString])
                 this.isThinking = false;
             },
       });
