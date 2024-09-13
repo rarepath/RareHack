82086ce63ee9136cbd621d69917c09438b728386
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2024-09-13 00:16:44 -0400

    bug fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ChatComponent } from './chat/chat.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { WhitepaperComponent } from './whitepaper/whitepaper.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'chat', component: ChatComponent },
     // { path: 'about-us', component: AboutUsComponent},
-    { path: 'whitepaper', component: WhitepaperComponent},
-    {path: ''}
+    { path: 'whitepaper', component: WhitepaperComponent}
 ];
 
 @NgModule({
