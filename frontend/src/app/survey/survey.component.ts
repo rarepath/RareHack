9caaf8652e758bfde1ab44e3eb427f47ef6cbe8f
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2024-09-13 00:43:13 -0400

    route back home in back action

--- frontend/src/app/survey/survey.component.ts
+++ frontend/src/app/survey/survey.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-survey',
@@ -9,7 +10,7 @@ import { Component } from '@angular/core';
 })
 export class SurveyComponent {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.openSurvey();
@@ -18,6 +19,7 @@ export class SurveyComponent {
   openSurvey(): void {
     const surveyUrl = 'https://oregonstate.qualtrics.com/jfe/form/SV_eRScOV0i4dDfmE6'; // Replace with your survey URL
     window.open(surveyUrl, '_blank');
+    this.router.navigate(['/']);  // Redirect back to home
   }
 
 }
